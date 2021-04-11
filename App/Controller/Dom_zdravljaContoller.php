04ffea83d0165e19dd130be4be85a45489b3cf47
Dinko <dinko.dugec@gmail.com>
2021-04-11 14:25:36 +0200

    implementacija rute za dom zdravlja

---
+++ App/Controller/Dom_zdravljaContoller.php
@@ -0,0 +1,15 @@
+<?php
+
+class Dom_zdravlja extends AutorizacijaController
+{
+    private $viewDir = 'privatno'
+                        . DIRECTORY_SEPARATOR
+                        . 'smjer'
+                        . DIRECTORY_SEPARATOR;
+
+    public function index()
+    {
+        $this->view->render($this->viewDir . 'index');
+    }
+
+}
