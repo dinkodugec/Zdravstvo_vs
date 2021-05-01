13141276b85fd31283412fdfc1802f62929718b7
Dinko <dinko.dugec@gmail.com>
2021-05-02 00:22:34 +0200

    Osnovna kontrola naziva

--- App/Controller/BolnicaController.php
+++ App/Controller/BolnicaController.php
@@ -20,9 +20,32 @@ class BolnicaController extends AutorizacijaController
 
     public function novo()
     {
-        $this->view->render($this->viewDir . 'novo',[
-            'bolnice'=>Bolnica::ucitajSve()
-        ]);
-    }
+        $this->view->render($this->viewDir . 'novo');
+        if($_SERVER['REQUEST_METHOD']==='GET'){
+            $bolnica = new stdClass();
+            $bolnica->naziv='';
+            $bolnica->ravnatelj='';
+            $bolnica->odjel='';
+            $bolnica->doktor='';
+            $this->view->render($this->viewDir . 'novo',[
+                'bolnica'=>$bolnica,
+                'poruka'=>'Popunite sve podatke'
+            ]);
+            return;
+        }
 
+    $bolnica = (object) $_POST;
+
+    if(strlen(trim($bolnica->naziv))===0){
+        $this->view->render($this->viewDir . 'novo',[
+            'bolnica'=>$bolnica,
+            'poruka'=>'Naziv obavezno'
+        ]);
+        return;
+    }
+      
+
+
+    }
+    
 }
