fc5aa9b505b1daa39a33f1b9b7b360eb01a8576e
Dinko <dinko.dugec@gmail.com>
2021-05-03 01:07:59 +0200

    Završen unos bolnice s kontrolama

--- App/Controller/BolnicaController.php
+++ App/Controller/BolnicaController.php
@@ -9,6 +9,8 @@ class BolnicaController extends AutorizacijaController
                         . DIRECTORY_SEPARATOR;
     
 
+     
+
 
     public function index()
     {
@@ -20,32 +22,81 @@ class BolnicaController extends AutorizacijaController
 
     public function novo()
     {
-        $this->view->render($this->viewDir . 'novo');
+       
         if($_SERVER['REQUEST_METHOD']==='GET'){
             $bolnica = new stdClass();
             $bolnica->naziv='';
             $bolnica->ravnatelj='';
             $bolnica->odjel='';
             $bolnica->doktor='';
-            $this->view->render($this->viewDir . 'novo',[
-                'bolnica'=>$bolnica,
-                'poruka'=>'Popunite sve podatke'
-            ]);
+            
+            $this->novoView($bolnica, 'Popunite sve podatke');
+            return;
+        }
+       
+        $bolnica = (object) $_POST;
+       
+        //naziv
+        if(strlen(trim($bolnica->naziv))===0){
+            $this->novoView($bolnica, 'Naziv obvezno');
+        return;
+        }
+
+        if(strlen(trim($bolnica->naziv))>50){
+            $this->novoView($bolnica, 'Naziv ne moze imati više od 50 znakova');
             return;
         }
 
-    $bolnica = (object) $_POST;
+        
+        //ravnatelj
+        if(strlen(trim($bolnica->ravnatelj))===0){
+            $this->novoView($bolnica, 'Ravnatelj obvezno');
+        return;
+        }
 
-    if(strlen(trim($bolnica->naziv))===0){
+        if(strlen(trim($bolnica->ravnatelj))>50){
+            $this->novoView($bolnica, 'Ravnatelj ne moze imati više od 50 znakova');
+            return;
+        }
+
+
+        //odjel
+        if(strlen(trim($bolnica->odjel))===0){
+            $this->novoView($bolnica, 'Odjel obvezno');
+        return;
+        }
+
+        if(strlen(trim($bolnica->odjel))>50){
+            $this->novoView($bolnica, 'Odjel ne moze imati više od 50 znakova');
+            return;
+        }
+        
+
+        //doktor
+        if(strlen(trim($bolnica->doktor))===0){
+            $this->novoView($bolnica, 'Doktor obvezno');
+        return;
+        }
+
+        if(strlen(trim($bolnica->doktor))>50){
+            $this->novoView($bolnica, 'Doktor ne moze imati više od 50 znakova');
+            return;
+        }
+
+
+        //ovdje sam siguran da je sve ok s kontrolama
+       Bolnica::dodajNovu($bolnica);
+       $this->index();
+        
+    }   
+    
+     private function novoView($bolnica, $poruka)
+    {
         $this->view->render($this->viewDir . 'novo',[
             'bolnica'=>$bolnica,
-            'poruka'=>'Naziv obavezno'
+            'poruka'=>'$poruka'
         ]);
-        return;
     }
-      
 
 
-    }
-    
-}
+}    
--- App/Model/Bolnica.php
+++ App/Model/Bolnica.php
@@ -16,4 +16,16 @@ class Bolnica
         return $izraz->fetchAll();
     }
 
+    public static function dodajNovu($bolnica)        //kada odradim sve kontrole
+    {                                                  // u bolnici
+        $veza = DB::getInstanca();
+        $izraz=$veza->prepare('
+        
+            insert into bolnica (naziv,ravnatelj,odjel,doktor)
+            values (:naziv,:ravnatelj,:odjel,:doktor)
+        
+        ');
+        $izraz->execute((array)$bolnica);
+    }
+
 }
