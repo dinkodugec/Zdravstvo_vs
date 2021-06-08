8c3685acc9f63ed7a6fd5fc6b5745f17ab721856
Dinko <dinko.dugec@gmail.com>
2021-06-08 21:30:50 +0200

    App isparavak case sensitive

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
 //echo __DIR__;
 define('BP',__DIR__ . DIRECTORY_SEPARATOR);
 define('BP_APP',__DIR__ . DIRECTORY_SEPARATOR 
-            . 'app' . DIRECTORY_SEPARATOR);
+            . 'App' . DIRECTORY_SEPARATOR);
 
 //echo BP;
 
